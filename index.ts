f4c31a06f3a0a90667bc2fff4babc7bb0a4323f5
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2020-10-16 14:22:19 +0200

    Binary search tree and linear search.

--- index.ts
+++ index.ts
@@ -134,7 +134,7 @@ stack.push({ value: -2 });
 stack.print();
 console.log(stack.size());
 
-console.log(`====== QUEUE`)
+console.log(`====== QUEUE`);
 const queue = new MyQueue();
 queue.print();
 queue.enqueue({ value: 1 });
@@ -157,3 +157,96 @@ queue.print();
 queue.enqueue({ value: 10 })
 queue.print();
 console.log(queue.size());
+
+
+console.log(`====== BINARY`);
+type BNode = {
+  value: number;
+  left: BNode | null;
+  right: BNode | null;
+}
+function binarySearchTree(curr: BNode, v:number): boolean {
+  if(curr === null) {
+    return false
+  }
+  if(curr.value === v) {
+    return true;
+  }
+  if(curr.value > v) {
+    return binarySearchTree(curr.left, v);
+  }
+  return binarySearchTree(curr.right, v);
+}
+function binarySearchList(arr: number[], v:number): number {
+  let L = 0;
+  let R = arr.length-1;
+
+  while(L <= R) {
+    const idx = L + Math.floor((R - L) / 2);
+    const value = arr[idx];
+    if(value === v) {
+      return idx;
+    }
+    if(value > v) {
+      R = idx - 1;
+    }
+    if(value < v) {
+      L = idx + 1;
+    }
+  }
+
+  return -1;
+}
+
+const list = [1, 5, 7, 9, 11, 15, 17];
+
+console.log(binarySearchList(list, 1));
+console.log(binarySearchList(list, 13));
+console.log(binarySearchList(list, 17));
+
+const head: BNode = {
+  value: 10,
+  left: {
+      value: 8,
+      left: {
+          value: 3,
+          right: null,
+          left: {
+              value: 1,
+              right: null,
+              left: null
+          }
+      },
+      right: {
+          value: 9,
+          right: null,
+          left: null,
+      }
+  },
+  right: {
+      value: 12,
+      left: {
+          value: 11,
+          left: null,
+          right: null,
+      },
+      right: {
+          value: 15,
+          left: {
+              value: 14,
+              left: {
+                  value: 13,
+                  left: null,
+                  right: null,
+              },
+              right: null
+          },
+          right: null,
+      }
+  }
+};
+console.log(binarySearchTree(head, 1));
+console.log(binarySearchTree(head, 13));
+console.log(binarySearchTree(head, 17));
+
+
