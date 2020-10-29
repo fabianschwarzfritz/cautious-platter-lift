007811e18fe3540568600b8c493a17cc7ff41285
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2020-10-29 14:39:16 +0100

    Implement simple version of chapter 1.1

--- chapter1.ts
+++ chapter1.ts
@@ -6,6 +6,14 @@
 // reduces to = O( n * x * (n+1)/2 )
 // reduces to = O(x * n^2)
 
+function printNewChapter(chapter: string, text: string) {
+    console.log("============================")
+    console.log(chapter)
+    console.log("---")
+    console.log(text)
+}
+
+
 class StringBuffer {
     private newPointer: number; 
     private buffer: string[];
@@ -63,3 +71,32 @@ for (let index = 0; index < 20; index++) {
     buffer.append(index + "");
 }
 console.log(buffer.toString());
+
+
+printNewChapter("Exercise 1.1", `
+Implement an algorithm to determine if a string has all unique characters.
+What if you cannot use additional data structures?
+`);
+
+// We start to iterate over the string and store each character into a hashmap.
+// In case this hash value has already a value, we know it already exists.
+const goodInput = "123456";
+const badInput = "1234346";
+
+function uniqueCharacters(input: string): boolean {
+    const arr = new Array();
+    for(let i = 0; i < input.length; i++) {
+        const char = input[i];
+        const hash = char.charCodeAt(0);
+        if(arr[hash] === true) {
+            return false;
+        }
+        arr[hash] = true;
+    }
+    return true;
+}
+
+console.log("Time complexity: O(n). Space complexity: O(1)");
+console.log(uniqueCharacters(goodInput));
+console.log(uniqueCharacters(badInput));
+
