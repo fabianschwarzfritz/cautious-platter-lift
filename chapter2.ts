31f80a18398d02da2a8c39a1371e69c15c26ad06
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2021-04-29 21:54:42 +0200

    Chapter 2.5 done

--- chapter2.ts
+++ chapter2.ts
@@ -260,8 +260,6 @@ You have two numbers represented by a linked liist, where each node contains
 a single digit. The digits are stored iin reverse order, such that the 1's digit
 is at the head of the list. Write a function that adds the two numbers and returns
 the sum as a linked list.
-
-Problem is WIP
 `);
 function sum(a: ListNode, b: ListNode) {
   let pa: ListNode = a;
